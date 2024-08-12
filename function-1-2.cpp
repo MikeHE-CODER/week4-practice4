38a17967c9ae0d9b161700798e0c2c0da713b1cf
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-12 14:58:50 +0930

    Update function-1-2.cpp

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -6,7 +6,7 @@ PersonList createPersonList(int n) {
   array.numPeople = n;
   array.people = new Person[n];
   for (int i = 0; i < n; i++) {
-    array.people[i].name = "Jane";
+    array.people[i].name = "Jane Doe";
     array.people[i].age = 1;
   }
   return array;
