35e1f28b967b8827448f8a19902e2fc0ebce169f
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-12 10:37:00 +0930

    Update function-1-2.cpp

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,10 +1,6 @@
 #include "Person.h"
 #include <iostream>
 #include <string>
-struct PersonList {
-  Person *people;
-  int numPeople;
-};
 PersonList createPersonList(int n) {
   PersonList *array = new PersonList[n];
   for (int i = 0; i < n; i++) {
